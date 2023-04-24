77d76933fe7ab74be968725a3558002bc7cf9b22
Wonyoung Choi <75527841+wnyoung0@users.noreply.github.com>
2023-04-24 20:36:38 +0900

    Create example.py

---
+++ example.py
@@ -0,0 +1,30 @@
+import magicArray as ma
+import random
+from copy import deepcopy
+ma.config('config.json')
+
+mm=ma.ArrayManager()
+
+
+for i in range(0,100):
+    mm.addArray(ma.MagicArray())
+    mm.getArray(i).setArray(ma.genArray())
+
+def f():
+    mm.sortList()
+    for i in range(0,100):
+        print(f"{i+1} {mm.getArray(i).getScore()}\n{mm.getArray(i).getArray()[0:3]}\n{mm.getArray(i).getArray()[3:6]}\n{mm.getArray(i).getArray()[6:9]}\n\n")
+    print("="*20)
+    for i in range(10,50):
+        ind=random.randint(0,9)
+        mm.getArray(i).setArray(ma.mutate(deepcopy(mm.getArray(ind).getArray())))
+    for i in range(50,100):
+        mm.getArray(i).setArray(ma.genArray())
+for i in range(0,40):
+    if mm.getArray(0).getScore()<=0.0:
+        print("=="*10)
+        print(f"\n{mm.getArray(0).getArray()[0:3]}\n{mm.getArray(0).getArray()[3:6]}\n{mm.getArray(0).getArray()[6:9]}\n\n")
+        break
+    print(i+1)
+    print()
+    f()
