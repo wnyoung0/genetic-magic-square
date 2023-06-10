fdd72a387640814de2890158cef0da0f8051003c
Wonyoung Choi <75527841+wnyoung0@users.noreply.github.com>
2023-06-10 17:00:35 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,13 +3,13 @@ import random
 from copy import deepcopy
 import json
 import matplotlib.pyplot as plt
-ma.config('data/star_6.json')
+ma.config('data/lat2in5.json')
 mm=ma.ArrayManager()
 
 dataF=open('data/data.json','r')
 data=json.load(dataF)
  
-
+ort=[5, 2, 2, 5, 1, 3, 3, 4, 1, 4, 1, 4, 2, 3, 5, 5, 3, 2, 4, 1, 1, 3, 5, 2, 4]
 
 def logg(i):
     global mm
@@ -26,27 +26,69 @@ def print_star6(i):
     plt.show()
 
 
-
 def f():
     mm.sortList()
-    for i in range(0,100):
+    for i in range(0,10):
         print(i+1,end=' ')
         print(mm.getArray(i).getScore(),mm.getArray(i).getArray(),sep='   ')
         print("="*20)
-    for i in range(10,50):
-        ind=random.randint(0,9)
-        mm.getArray(i).setArray(ma.mutate(deepcopy(mm.getArray(ind).getArray())))
-    for i in range(50,100):
+    for i in range(40,70):
+        ind=random.randint(0,10)
         mm.getArray(i).setArray(ma.mutate(ma.mutate(deepcopy(mm.getArray(ind).getArray()))))
-       
-for i in range(0,100):
+    for i in range(70,90):
+        ind=random.randint(0,40)
+        mm.getArray(i).setArray(ma.mutate(ma.mutate(ma.mutate(deepcopy(mm.getArray(ind).getArray())))))
+    for i in range(90,130):
+        ind=random.randint(0,40)
+        lLLlL=ma.mutate(ma.mutate(deepcopy(mm.getArray(ind).getArray())))
+        for i in range(20):
+            lLLlL=ma.mutate(lLLlL)
+        mm.getArray(i).setArray(lLLlL)
+    for i in range(130,180):
+        ind=random.randint(0,30)
+        mm.getArray(i).setArray(ma.permitate(mm.getArray(ind).getArray(),5))
+    for i in range(180,300):
+        ind=random.randint(0,5)
+        mm.getArray(i).setArray(ma.mutate(ma.mutate(ma.mutate(ma.mutate(ma.mutate(deepcopy(mm.getArray(ind).getArray())))))))
+    for i in range(300,390):
+        ind=random.randint(0,5)
+        mm.getArray(i).setArray(ma.mutate(ma.mutate(deepcopy(mm.getArray(ind).getArray()))))
+    for i in range(390,500):
+        mm.getArray(i).setArray(deepcopy(ma.defaultArray))
+def ff(n):
+    for i in range(0,n):
+        print("j",end='')
+        print()
+        print()
+        mm.getArray(i).setArray(deepcopy(ma.defaultArray))
+
+for i in range(0,500):
     mm.addArray(ma.MagicArray())
-    mm.getArray(i).setArray(ma.genArray())
-for i in range(0,140):
-    if mm.getArray(0).getScore()<=0.0:
+    mm.getArray(i).setArray(deepcopy(ma.defaultArray))
+    mm.getArray(i).setOrth(ort)
+
+
+
+rec=0
+ccc=0
+lLlllL=[]
+for i in range(0,1000):
+
+    if mm.getArray(0).getScore()<=0.0 and ma.isMagic(mm.getArray(0).getArray(),ort):
         print("||"*10+'\n',sep='')
-        print_star6(0)
+        print(f"\n{mm.getArray(0).getArray()[0:6]}\n{mm.getArray(0).getArray()[6:12]}\n{mm.getArray(0).getArray()[12:18]}\n{mm.getArray(0).getArray()[18:24]}\n{mm.getArray(0).getArray()[24:30]}\n{mm.getArray(0).getArray()[30:36]}")
         break
+    print(f"---{ccc}")
+    
+    lLlllL=deepcopy(mm.getArray(0).getArray())
+    if rec==mm.getArray(0).getArray():
+        ccc+=1
+    else:
+        ccc=0
+    if ccc>=100:
+        ff(480)
+   
+    rec=deepcopy(lLlllL)
     print(i+1)
     print()
     f()
